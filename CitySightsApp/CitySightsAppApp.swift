bef7e520611e7a564ce3fb5379aa04acdc767675
Michael Chavez <mhchavez99@gmail.com>
2021-06-24 13:09:57 -0500

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Michael Chavez on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySightsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CitySightsApp
+//
+//  Created by Michael Chavez on 6/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
